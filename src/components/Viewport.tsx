1caab006996665ee5e3e1fcd6a73d8d182687799
Raúl Pleitez <racsoraul@gmail.com>
2019-07-23 20:00:54 -0600

    Base Viewport component.

---
+++ src/components/Viewport.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Viewport extends Component<{}> {
+  constructor(props: {}) {
+    super(props);
+  }
+
+  render() {
+    return <div>Viewport</div>;
+  }
+}
+
+export default Viewport;
