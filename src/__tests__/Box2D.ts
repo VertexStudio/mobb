332170dde4e5a7c2478760fdef0bbeacf5a9e1b8
Raúl Pleitez <racsoraul@gmail.com>
2019-07-26 01:06:28 +0000

    Adds Box2D model.

---
+++ src/__tests__/Box2D.ts
@@ -0,0 +1,88 @@
+import Box2D from "../models/Box2D";
+import Vector2D from "../models/Vector2D";
+
+it("new uninitialized-box", () => {
+  const NewBox = new Box2D();
+
+  expect(NewBox.IsValid).toBe(false);
+  expect(NewBox.Min).toEqual(Vector2D.ZeroVector);
+  expect(NewBox.Max).toEqual(Vector2D.ZeroVector);
+});
+
+it("new valid initialized-box", () => {
+  const NewBox = new Box2D("A", new Vector2D(10, 15), new Vector2D(22, 19));
+
+  expect(NewBox.IsValid).toBe(true);
+  expect(NewBox.Min).not.toEqual(Vector2D.ZeroVector);
+  expect(NewBox.Max).not.toEqual(Vector2D.ZeroVector);
+});
+
+it("new invalid initialized-box", () => {
+  const NewBox = new Box2D("A", new Vector2D(10, 15), new Vector2D(7, 14));
+
+  expect(NewBox.IsValid).toBe(false);
+});
+
+it("box equality", () => {
+  const Box1 = new Box2D();
+  const Box2 = new Box2D();
+
+  expect(Box1.IsEqual(Box2)).toBe(true);
+
+  Box1.Min = new Vector2D(12, 16);
+
+  expect(Box1.IsEqual(Box2)).toBe(false);
+
+  Box2.Min = new Vector2D(7, 20);
+
+  expect(Box1.IsEqual(Box2)).toBe(false);
+
+  Box1.Max = new Vector2D(15, 22);
+  Box2.Max = new Vector2D(15, 22);
+
+  expect(Box1.IsEqual(Box2)).toBe(false);
+
+  Box1.Min = new Vector2D(2, 7);
+  Box2.Min = new Vector2D(2, 7);
+
+  expect(Box1.IsEqual(Box2)).toBe(true);
+});
+
+it("check box validity", () => {
+  const NewBox = new Box2D();
+
+  expect(NewBox.IsValid).toBe(false);
+
+  NewBox.Max = new Vector2D(42, 23);
+
+  expect(NewBox.IsValid).not.toBe(false);
+
+  NewBox.Max = new Vector2D(-42, 23);
+
+  expect(NewBox.IsValid).toBe(false);
+
+  NewBox.Min = new Vector2D(-43, 22);
+
+  expect(NewBox.IsValid).toBe(true);
+});
+
+it("overlapping and non-overlapping boxes", () => {
+  const BoxA = new Box2D("A", Vector2D.ZeroVector, new Vector2D(20, 20));
+  const BoxB = new Box2D("B", new Vector2D(5, 6), new Vector2D(20, 20));
+
+  expect(BoxA.OverlappingAreaWith(BoxB)).toBeGreaterThan(0);
+
+  BoxA.Min = new Vector2D(50, 50);
+  BoxA.Max = new Vector2D(60, 60);
+  BoxB.Min = new Vector2D(10, 10);
+  BoxB.Max = new Vector2D(30, 30);
+
+  expect(BoxA.OverlappingAreaWith(BoxB)).toBe(0);
+
+  BoxA.Min = new Vector2D(50, 50);
+  BoxA.Max = new Vector2D(60, 60);
+  BoxB.Min = new Vector2D(50, 50);
+  BoxB.Max = new Vector2D(55, 55);
+
+  expect(BoxA.OverlappingAreaWith(BoxB)).toBe(25);
+});
---
+++ src/models/Box2D.ts
@@ -0,0 +1,105 @@
+import Vector2D from "./Vector2D";
+
+class Box2D {
+  private _Name: string;
+  private _DistanceFromCameraView: number;
+  private _Min: Vector2D;
+  private _Max: Vector2D;
+  private _IsValid: boolean;
+
+  constructor();
+  constructor(Name: string, Min: Vector2D, Max: Vector2D);
+  constructor(Name?: any, Min?: any, Max?: any) {
+    this._DistanceFromCameraView = 0;
+    this._IsValid = false;
+    if (
+      typeof Name === "string" &&
+      Min instanceof Vector2D &&
+      Max instanceof Vector2D
+    ) {
+      this._Name = Name;
+      this._Min = Min;
+      this._Max = Max;
+      this.SetBoxValidity();
+    } else {
+      this._Name = `${Date.now()}`;
+      this._Min = Vector2D.ZeroVector;
+      this._Max = Vector2D.ZeroVector;
+      this._IsValid = false;
+    }
+  }
+
+  public get Name(): string {
+    return this._Name;
+  }
+
+  public get DistanceFromCameraView(): number {
+    return this._DistanceFromCameraView;
+  }
+
+  public get Min(): Vector2D {
+    return this._Min;
+  }
+
+  public get Max(): Vector2D {
+    return this._Max;
+  }
+
+  public get IsValid(): boolean {
+    return this._IsValid;
+  }
+
+  public set Name(v: string) {
+    this._Name = v;
+  }
+
+  public set DistanceFromCameraView(v: number) {
+    this._DistanceFromCameraView = v;
+  }
+
+  public set Min(v: Vector2D) {
+    this._Min.X = v.X;
+    this._Min.Y = v.Y;
+    this.SetBoxValidity();
+  }
+
+  public set Max(v: Vector2D) {
+    this._Max.X = v.X;
+    this._Max.Y = v.Y;
+    this.SetBoxValidity();
+  }
+
+  /**
+   * Checks if Min and Max points are valid and sets validity.
+   */
+  private SetBoxValidity(): void {
+    this._IsValid = this._Min.X < this._Max.X && this._Min.Y < this._Max.Y;
+  }
+
+  /**
+   * Checks if boxes are equal.
+   * @param Box to compare against
+   */
+  IsEqual(Box: Box2D): boolean {
+    return this._Min.IsEqual(Box.Min) && this._Max.IsEqual(Box.Max);
+  }
+
+  /**
+   * Calcs overlapping area with the given box.
+   * @param Box to check against
+   */
+  OverlappingAreaWith(Box: Box2D): number {
+    const IntersectionX =
+      Math.min(this._Max.X, Box._Max.X) - Math.max(this._Min.X, Box.Min.X);
+    const IntersectionY =
+      Math.min(this._Max.Y, Box.Max.Y) - Math.max(this._Min.Y, Box._Min.Y);
+
+    if (IntersectionX >= 0 && IntersectionY >= 0) {
+      return IntersectionX * IntersectionY;
+    }
+
+    return 0;
+  }
+}
+
+export default Box2D;
