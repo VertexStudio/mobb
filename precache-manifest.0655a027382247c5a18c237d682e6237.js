4644d2522dcb033b22aaabf19cca8e94b8ed1361
racsoraul <racsoraul@gmail.com>
2019-07-29 19:53:23 +0000

    Updates

---
+++ precache-manifest.0655a027382247c5a18c237d682e6237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427e7617361e2aab5e17450621c250a3",
+    "url": "/mobb/index.html"
+  },
+  {
+    "revision": "14b48ef8150aabae2bbc",
+    "url": "/mobb/static/css/main.7f8e4208.chunk.css"
+  },
+  {
+    "revision": "7e637047be283ed9bfa2",
+    "url": "/mobb/static/js/2.1ddabbbf.chunk.js"
+  },
+  {
+    "revision": "14b48ef8150aabae2bbc",
+    "url": "/mobb/static/js/main.c6b47bbe.chunk.js"
+  },
+  {
+    "revision": "f218dc1e8a052ec1dd47",
+    "url": "/mobb/static/js/runtime~main.458d20e2.js"
+  }
+]);
